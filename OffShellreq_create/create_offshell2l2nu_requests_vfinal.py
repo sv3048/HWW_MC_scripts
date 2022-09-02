8bba6701bfc5a37bfe3cb27f190a2dd351708714
SADHANA VERMA <52027489+sv3048@users.noreply.github.com>
2022-09-02 11:13:43 +0530

    Update create_offshell2l2nu_requests_vfinal.py

--- OffShellreq_create/create_offshell2l2nu_requests_vfinal.py
+++ OffShellreq_create/create_offshell2l2nu_requests_vfinal.py
@@ -23,18 +23,18 @@ def get_requests_from_datasetname(dn):
     return result
 
 # M = 200 GeV is not below because corresponding requests were already created
-#masses = [  '160', '170', '180', '190', '210', '230', '250', '270', '300', '350', '400', '450', '500', '550', 
+#masses = [  '125', '160', '170', '180', '190', '210', '230', '250', '270', '300', '350', '400', '450', '500', '550', 
 #            '600', '700', '800', '900', '1000', '1500', '2000', '2500', '3000']
 
- # M = 200 GeV is not below because corresponding requests were already created
-masses = [  '160', '170']
-prepid_M200_20UL16 = {  'GGH' : 'HIG-RunIISummer20UL16wmLHEGEN-11100', 
+masses = [  '600', '700']
+
+prepid_M200_20UL16 = {  'GGH' : 'HIG-RunIISummer20UL16wmLHEGEN-11100',
                         'GGH_minloHJJ' : '', # minloHJJ still has to be created
                         'VBFH' : 'HIG-RunIISummer20UL16wmLHEGEN-11929',
                         'ZH_LNuQQ_2LFilter' : 'HIG-RunIISummer20UL16wmLHEGEN-11931',
-                        'ZH_LNuQQ' : 'HIG-RunIISummer20UL16wmLHEGEN-11932', 
-                        'WminusH_2LOSFilter' : 'HIG-RunIISummer20UL16wmLHEGEN-11933', 
-                        'WplusH_2LOSFilter' : 'HIG-RunIISummer20UL16wmLHEGEN-11934', 
+                        'ZH_LNuQQ' : 'HIG-RunIISummer20UL16wmLHEGEN-11932',
+                        'WminusH_2LOSFilter' : 'HIG-RunIISummer20UL16wmLHEGEN-11933',
+                        'WplusH_2LOSFilter' : 'HIG-RunIISummer20UL16wmLHEGEN-11934',
                         'ZH' : 'HIG-RunIISummer20UL16wmLHEGEN-11935'
                     }
 
@@ -43,33 +43,19 @@ for mode, pi in prepid_M200_20UL16.items():
     if mode == 'GGH_minloHJJ':
         continue
     req = mcm.get('requests', pi)
+    dataset_name = req['dataset_name']
+    fragment = req['fragment']
     for mass in masses:
-        print mass
-        '''
-        req['dataset_name'] = req['dataset_name'].replace('M-200', 'M-' + mass)
+        req['dataset_name'] = dataset_name.replace('M-200', 'M-' + mass)
         print req['dataset_name']
-        #req['member_of_campaign'] = 'RunIISummer20UL16wmLHEGENAPV'
+        req['member_of_campaign'] = 'RunIISummer20UL16wmLHEGENAPV'
         #req['member_of_campaign'] = 'RunIISummer20UL17wmLHEGEN'
         #req['member_of_campaign'] = 'RunIISummer20UL18wmLHEGEN'
-        req['fragment'] = req['fragment'].replace('M200', 'M' + mass)
-        #print req['fragment']
+        req['fragment'] = fragment.replace('M200', 'M' + mass)
+        print req['fragment']
         if '17' in req['member_of_campaign'] or '18' in req['member_of_campaign']:
             req['total_events'] = 500000
-        mcm.update('requests', req)
-        
-       '''
-        new_req  = mcm.clone_request(req)
-        new_req['dataset_name'] = new_req['dataset_name'].replace('M-200', 'M-' + mass)
-        #print req['dataset_name']
-        #req['member_of_campaign'] = 'RunIISummer20UL16wmLHEGENAPV'
-        #req['member_of_campaign'] = 'RunIISummer20UL17wmLHEGEN'
-        #req['member_of_campaign'] = 'RunIISummer20UL18wmLHEGEN'
-        new_req['fragment'] = new_req['fragment'].replace('M200', 'M' + mass)
-        #print req['fragment']
-        if '17' in req['member_of_campaign'] or '18' in req['member_of_campaign']:
-            req['total_events'] = 500000
-        new_req = mcm.update('requests', new_req)
-        print new_req
-        mcm.approve('requests', new_req['prepid'], 0)
-        
+        new_req                     = mcm.clone_request(req)
+        mcm.update('requests', new_req)
+        mcm.approve('requests', new_req['prepid'], 1)
         
