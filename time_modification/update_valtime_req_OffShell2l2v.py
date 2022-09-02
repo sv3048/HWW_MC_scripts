aed784786ada99a08ff1c078dc96873bcca0d4ff
SADHANA VERMA <52027489+sv3048@users.noreply.github.com>
2022-09-03 01:56:02 +0530

    Create update_valtime_req_OffShell2l2v.py

---
+++ time_modification/update_valtime_req_OffShell2l2v.py
@@ -0,0 +1,44 @@
+import sys
+import json
+sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
+from rest import McM
+from collections import defaultdict
+from collections import OrderedDict
+import numpy as np
+import pandas as pd
+
+
+mcm     = McM(dev = False, debug = True)
+
+
+#import list of requests need to be deleted
+import prepid_2update
+from prepid_2update  import ls
+
+
+def get_request(prepid):
+   result  = mcm._McM__get('public/restapi/requests/get/%s' % (prepid))
+   if not result:
+        return {}
+   result  = result.get('results', {})
+   return result
+
+def get_requests_from_datasetname(dn):
+    result  = mcm.get('requests', query='dataset_name=%s' % (dn))
+    if not result:
+        return {}
+    return result
+
+
+for  pid in ls:
+    print pid
+    req = mcm.get('requests', pid)
+    print req['validation']
+    if req==None :
+      print( "request don't exist")
+    else:
+      #req['validation'] =  # Why its wowrking with squrare brackets ?
+      #print req['validation']
+      #mcm.update('requests', req)
+      #request2 = mcm.get('requests', pid)
+
